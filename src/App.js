ff3b82aa85f544518dc6bd7d36d688e9231c30ef
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-04-27 19:43:43 +0530

    Blogs with React Ninja completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/create" element={<Create />} />
+          <Route exact path="/blogs/:id" element={<BlogDetails />} />
+          <Route exact path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/NotFound.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>404 NotFound</h2>
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default NotFound;
